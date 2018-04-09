6e8de2e4f164624bea5495f2b383508df733147b
ue <yoshi.maximum@gmail.com>
2018-04-09 22:21:18 +0900

    fixed PriorityProvider.cpp

--- Src/Modules/BehaviorControl/Priority/PriorityProvider.cpp
+++ Src/Modules/BehaviorControl/Priority/PriorityProvider.cpp
@@ -29,11 +29,26 @@ MAKE_MODULE(PriorityProvider, behaviorControl)
   }
 
 void PriorityProvider::calcPriority(){
+	float x, y, d, theta, dis, dir;
+	x = theBallModel.estimate.position.x();
+	y = theBallModel.estimate.position.y();
+	theta = (theRobotPose.inverse() * Vector2f(theFieldDimensions.xPosOpponentGroundline, 0.f)).angle();
+
+	x /= 500.f;
+	y /= 500.f;
+	d = (float)sqrt(pow(x, 2)+pow(y, 2));
+
+	if(d < 1.f){
+		d = 1.0f;
+	}
+	dir = (float)((cos(theta)+1.0)/2.0);
+	dis = 1.0f/d;
+	priority[theRobotInfo.number] = (alpha*dir + beta*dis)/(alpha+beta);
+
 
 	for(auto const& teammate : theTeammateData.teammates){
-		float x, y, d, theta, dis, dir;
 
-		if( !teammate.isPenalized || timeSinceBallWasSeen() > 3000 ){
+		if( teammate.status == Teammate::PLAYING || timeSinceBallWasSeen() > 3000 ){
 
 			x = teammate.ball.estimate.position.x();
 			y = teammate.ball.estimate.position.y();
@@ -129,6 +144,23 @@ Vector2f PriorityProvider::calcSupportPose(){
 	int stRank = getoneRank();
 
 	float dis = 3000.f;
+
+	if(theRobotInfo.number == stRank){
+		if(0.7 < theRobotPose.validity){
+			distiX = (float)(theRobotPose.translation.x() + cos(theRobotPose.rotation)*(dis));
+			distiY = (float)(theRobotPose.translation.y() + sin(theRobotPose.rotation)*(dis));
+		}
+		else{
+			distiX = (float)(theBallModel.estimate.position.x() + dis);
+			distiY = (float)(theBallModel.estimate.position.y());
+		}
+		if(distiX > 4000.f) distiX = 4000.f;
+		if(distiY > 2500.f) distiY = 2500.f;
+
+		return (Vector2f(distiX,distiY));
+	}
+
+
 	for(auto const& teammate : theTeammateData.teammates){
 		if(teammate.number == stRank){
 			if(0.7 < teammate.pose.validity){
